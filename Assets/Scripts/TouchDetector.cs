d7f0da40455fffbe76e4c692bc3b34fdf1203814
negaft <negaft@yandex.ru>
2014-11-06 19:53:18 +0700

    Test

--- Assets/Scripts/TouchDetector.cs
+++ Assets/Scripts/TouchDetector.cs
@@ -9,7 +9,18 @@ public class TouchDetector : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (Input.touchCount != 0) 
+		{
+			RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position), Vector2.zero);
+			if (hit.collider) 
+			{ 
+				if (hit.collider == this.gameObject.collider2D) 
+				{
+					Application.LoadLevel (1);
+				}
+			}
+		}
 	}
 }
